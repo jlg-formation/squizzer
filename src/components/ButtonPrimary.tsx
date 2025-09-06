937df08b6ca484266decb085951ea4e06abfc764
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-09-06 17:36:33 +0200

    feat: enhance ButtonPrimary component layout with flexbox for improved alignment

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -16,7 +16,7 @@ const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
   ...props
 }) => (
   <button
-    className={`cursor-pointer rounded border border-black bg-black px-4 py-2 font-mono text-white shadow transition-colors duration-150 hover:bg-gray-900 ${disabled ? "cursor-not-allowed border-gray-400 bg-gray-300 text-gray-500 opacity-50" : ""} ${className}`}
+    className={`flex cursor-pointer items-center justify-center rounded border border-black bg-black px-4 py-2 font-mono text-white shadow transition-colors duration-150 hover:bg-gray-900 ${disabled ? "cursor-not-allowed border-gray-400 bg-gray-300 text-gray-500 opacity-50" : ""} ${className}`}
     {...props}
     disabled={disabled}
   >
